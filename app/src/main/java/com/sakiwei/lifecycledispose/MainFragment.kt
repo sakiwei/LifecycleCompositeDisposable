718c95e8b985a47fcb41ae818286fb48710fdf88
Wai Cheung <sakiwei@gmail.com>
2017-08-05 22:00:51 +0800

    ADDED: LifecycleFragment example

--- app/src/main/java/com/sakiwei/lifecycledispose/MainFragment.kt
+++ app/src/main/java/com/sakiwei/lifecycledispose/MainFragment.kt
@@ -1,23 +1,30 @@
 package com.sakiwei.lifecycledispose
 
-import android.arch.lifecycle.LifecycleActivity
+import android.arch.lifecycle.LifecycleFragment
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import io.reactivex.Observable
 import java.util.concurrent.TimeUnit
 
-class MainActivity : LifecycleActivity() {
 
-    val TAG = "MainActivity"
+class MainFragment: LifecycleFragment() {
+
+    val TAG = "MainFragment"
     // create a LifecycleCompositeDisposable which will be disposed at onPause()
     val disposableBag = createOnPauseCompositeDisposable()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater?.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
         // keep firing a number on each second,
-        // until LifecycleActivity.onPause() is triggered
+        // until LifecycleFragment.onPause() is triggered
         Observable.interval(1000, TimeUnit.MILLISECONDS)
                 .doOnDispose {
                     Log.d(TAG, "Disposed!")
